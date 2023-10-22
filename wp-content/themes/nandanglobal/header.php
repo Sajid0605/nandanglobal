e29af04deaf1547bde4d512b74058fc70fb38663
Sajid Ansari <sajidansari0605@gmail.com>
2023-10-23 00:24:06 +0530

    added theme, ACF field and Header completed

---
+++ wp-content/themes/nandanglobal/header.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Nandan_Global
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<?php
+	 global $current_user; wp_get_current_user();
+?>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site container">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'nandanglobal' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<nav class="navbar navbar-expand-lg bg-body-tertiary rounded" aria-label="Thirteenth navbar example">
+			<div class="container-fluid">
+				<div class="collapse navbar-collapse d-lg-flex">
+					<?php
+						if ( is_front_page() ) :
+							?>
+							<h1 class="site-title navbar-brand col-lg-3 justify-content-lg-end"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+							<?php
+						else :
+							?>
+							<p class="site-title navbar-brand col-lg-3 justify-content-lg-end"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+							<?php
+						endif;
+					?>
+
+					<?php
+						wp_nav_menu(
+							array(
+								'menu_class' => 'navbar-nav col-lg-6 justify-content-lg-center',
+								'container' => false,
+								'theme_location' => 'main',
+								'items_wrap' => '
+										<ul class="%2$s">
+											%3$s
+										</ul>
+										',
+								'walker' => new NG_Nav_Menu_Walker_Simple
+							)
+						);
+					?>
+					
+					<?php if(is_user_logged_in()): ?>
+						<div class="d-lg-flex col-lg-3 justify-content-lg-center">
+							<ul class="navbar-nav">
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false"><?php echo $current_user->display_name; ?></a>
+									<ul class="dropdown-menu">										
+										<li><a class="dropdown-item" href="<?php echo wp_logout_url(); ?>">Log Out</a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					<?php endif; ?>
+				</div>
+			</div>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
---
+++ wp-content/themes/nandanglobal/inc/customizer.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * Nandan Global Theme Customizer
+ *
+ * @package Nandan_Global
+ */
+
+
+add_image_size( 'favicon_img_size', 32, 32 );
+
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function nandanglobal_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial(
+			'blogname',
+			array(
+				'selector'        => '.site-title a',
+				'render_callback' => 'nandanglobal_customize_partial_blogname',
+			)
+		);
+		$wp_customize->selective_refresh->add_partial(
+			'blogdescription',
+			array(
+				'selector'        => '.site-description',
+				'render_callback' => 'nandanglobal_customize_partial_blogdescription',
+			)
+		);
+	}
+}
+add_action( 'customize_register', 'nandanglobal_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function nandanglobal_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function nandanglobal_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function nandanglobal_customize_preview_js() {
+	wp_enqueue_script( 'nandanglobal-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), _S_VERSION, true );
+}
+add_action( 'customize_preview_init', 'nandanglobal_customize_preview_js' );
+
+/**
+ * Enqueue Scripts & Style for themes
+ */
+function ng_enqueue_style_scripts_files() {
+
+	/* Adding Style */
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
+
+	/* Adding Script */
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array('jquery'), '1.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'ng_enqueue_style_scripts_files' );
+
+
+
+/**
+ * Navigation Menu
+ */
+function register_nav_menu_location(){
+    register_nav_menus(
+        array(
+            'main' => __( 'Main Menu', 'nandanglobal' ),
+        )
+    );
+}
+add_action( 'after_setup_theme', 'register_nav_menu_location' );
+
+
+/**
+ * Custom Menu Naviation
+ */
+class NG_Nav_Menu_Walker_Simple extends Walker_Nav_Menu
+{
+	/**
+	 * Start the element output.
+	 *
+	 * @param  string $output Passed by reference. Used to append additional content.
+	 * @param  object $item   Menu item data object.
+	 * @param  int $depth     Depth of menu item. May be used for padding.
+	 * @param  array $args    Additional strings.
+	 * @return void
+	 */
+	public function start_el( &$output, $item, $depth, $args )
+	{
+		$output     .= '<li class="nav-item">';
+		$attributes  = '';
+
+		! empty ( $item->attr_title )
+			// Avoid redundant titles
+			and $item->attr_title !== $item->title
+			and $attributes .= ' title="' . esc_attr( $item->attr_title ) .'"';
+
+		! empty ( $item->url )
+			and $attributes .= ' href="' . esc_attr( $item->url ) .'"';
+
+		$image_obj = get_field('menu_image', $item->ID);
+		$image_url = $image_obj['sizes']['favicon_img_size'];
+
+		$class_names = in_array("current_page_item",$item->classes) ? ' active' : '';
+
+		$attributes  = trim( $attributes );
+		$title       = apply_filters( 'the_title', $item->title, $item->ID );
+		$item_output = "<img src='$image_url' class='img-thumbnail d-block m-auto'>$args->before<a class='nav-link $class_names' $attributes>$args->link_before$title</a>"
+						. "$args->link_after$args->after";
+
+		// Since $output is called by reference we don't need to return anything.
+		$output .= apply_filters(
+			'walker_nav_menu_start_el'
+			,   $item_output
+			,   $item
+			,   $depth
+			,   $args
+		);
+	}
+
+	/**
+	 * @see Walker::start_lvl()
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @return void
+	 */
+	public function start_lvl( &$output )
+	{
+		$output .= '<ul>';
+	}
+
+	/**
+	 * @see Walker::end_lvl()
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @return void
+	 */
+	public function end_lvl( &$output )
+	{
+		$output .= '</ul>';
+	}
+
+	/**
+	 * @see Walker::end_el()
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @return void
+	 */
+	function end_el( &$output )
+	{
+		$output .= '</li>';
+	}
+}
